268aff25d2ad412e72248fbabbee6cebbcf76a66
wesleyDM14 <wesleyfga@hotmail.com>
2021-12-07 11:21:14 -0300

    Add SignIn and Home houtes

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import firebase from './src/services/firebaseconnection';
+
+import Routes from './src/routes/index';
 
 export default function App() {
  return (
-   <View/>
+   <NavigationContainer>
+     <StatusBar backgroundColor="#00ff00" barStyle="light-content"/>
+     <Routes/>
+   </NavigationContainer>
   );
 }
